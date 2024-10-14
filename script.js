8bff685197f46c5f3a15ed0759de0c0abd26f458
Biswojit Biswal <120778980+biswojitbiswal@users.noreply.github.com>
2024-10-14 08:53:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 //your JS code here. If required.
 function daysOfAYear(year){
-	if(year % 4 == 0 && (year % 100 == 0 && year % 400 != 0)){
-		console.log(366);
+	if(year % 4 == 0 ){
+		if(year % 100 == 0){
+			if(year % 400 == 0){
+				return 366;
+			} else {
+				return 365;
+			}
+		} else {
+			return 366;
+		}
 	} else {
-		console.log(365);
+		return 365;
 	}
 }
+daysOfAYear(1900);
 daysOfAYear()
